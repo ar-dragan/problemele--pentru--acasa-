ea58f40be01917ab08faf9df74df5fb3eb479ffe
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-09-23 15:29:40 +0300

    Add files via upload

---
+++ problema 2.py
@@ -0,0 +1,8 @@
+a=25
+b=10
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"//",b,"=",a//b)
+print(a,"%",b,"=",a%b)
+print(a,"*%,b,"=",a*b)
+print(a,"**",b,"=",a**b)
---
+++ problema3.py
@@ -0,0 +1,4 @@
+lungimea_laturii=4
+print("perimetrul_suprafetei","=",12*lungimea laturii,"metri")
+print("aria_tutor_suprafetelor","=",6*lungimea laturii**2,"metri_patrati")
+print("volumul_cubului","=",lungimea laturii**3,"metri_cubi")
